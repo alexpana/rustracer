662c7149aed4f6749e06646f327309dee63c9250
Alexandru Pana <alexandru.pana@manabreakstdios.com>
2017-02-21 21:16:14 +0200

    Success at rendering on multiple threads

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate rand;
+extern crate crossbeam;
 
 mod vec3;
 mod ray;
@@ -20,6 +21,7 @@ use std::thread;
 
 const T_MIN: f32 = 0.001;
 const T_MAX: f32 = f32::MAX;
+const SAMPLE_COUNT: usize = 300;
 
 fn color(ray: Ray, scene: &Scene) -> Vec3 {
     let hit = scene.hit(ray, T_MIN, T_MAX);
@@ -38,27 +40,36 @@ fn color(ray: Ray, scene: &Scene) -> Vec3 {
     }
 }
 
-fn trace(shared_camera: Arc<Camera>, scene: Arc<Scene>, image_buffer: &mut [u8]) {
-    const SAMPLE_COUNT: usize = 200;
+fn trace(shared_camera: Arc<Camera>, scene: Arc<Scene>, offset: usize, image_buffer: &mut [u8]) {
+    print!("Tracing at offset {} size {}:{}\n", offset, image_buffer.len(), image_buffer.len() / 3);
 
     let camera = &shared_camera;
 
-    for x in 0..camera.height {
-        for y in 0..camera.width {
-            let mut color_acc = vec3::ZERO;
+    for i in 0..(image_buffer.len() / 3) {
+        let x = (offset + i) / camera.width;
+        let y = (offset + i) % camera.width;
+        let mut color_acc = vec3::ZERO;
 
-            for sample in random_samples(SAMPLE_COUNT) {
-                let r = camera.ray(y as f32 + sample.0, x as f32 + sample.1);
-                color_acc = color_acc + color(r, &scene) * sample.2;
-            }
-            image_buffer[(x * camera.width + y) * 3 + 0] = (color_acc.x * 255.0) as u8;
-            image_buffer[(x * camera.width + y) * 3 + 1] = (color_acc.y * 255.0) as u8;
-            image_buffer[(x * camera.width + y) * 3 + 2] = (color_acc.z * 255.0) as u8;
+        for sample in random_samples(SAMPLE_COUNT) {
+            let r = camera.ray(y as f32 + sample.0, x as f32 + sample.1);
+            color_acc = color_acc + color(r, &scene) * sample.2;
         }
+
+        if i % 100 == 0 {
+            print!("offset: {} current index: {}\n", offset, i);
+        }
+
+        image_buffer[i * 3 + 0] = (color_acc.x * 255.0) as u8;
+        image_buffer[i * 3 + 1] = (color_acc.y * 255.0) as u8;
+        image_buffer[i * 3 + 2] = (color_acc.z * 255.0) as u8;
     }
+
+    print!("Done tracing at offset {}\n", offset);
 }
 
 fn write_ppm(image_buffer: &[u8], image_size: (usize, usize), file_name: &str) {
+    print!("Writing output image to {}\n", file_name);
+
     let file = File::create(file_name).expect("Could not open output file");
     write!(&file, "P3\n{} {}\n255\n", image_size.0, image_size.1);
 
@@ -74,7 +85,7 @@ fn write_ppm(image_buffer: &[u8], image_size: (usize, usize), file_name: &str) {
 }
 
 fn main() {
-    const THREAD_COUNT: usize = 4;
+    const THREAD_COUNT: usize = 8;
 
     let scene = Arc::new(Scene {
         objects: vec![
@@ -94,26 +105,20 @@ fn main() {
 
     let mut buffer = vec![0u8; camera.width * camera.height * 3];
 
-    {
+    crossbeam::scope(|scope| {
         let chunk_buffer_size = (camera.width * camera.height * 3) / THREAD_COUNT;
         let chunks: Vec<&mut [u8]> = buffer.chunks_mut(chunk_buffer_size).collect();
 
-//        let mut scope_guards = Vec::with_capacity(THREAD_COUNT);
-
         for (i, mut chunk) in chunks.into_iter().enumerate() {
             let start = i * chunk_buffer_size;
-            let size = chunk.len();
-            println!("{} {}\n", start, size);
             let shared_camera = camera.clone();
             let shared_scene = scene.clone();
 
-            let guard = thread::spawn(move || {
-                trace(shared_camera, shared_scene, &mut chunk);
+            scope.spawn(move || {
+                trace(shared_camera, shared_scene, start / 3, &mut chunk);
             });
-            guard.join();
-//            scope_guards.push(guard);
         }
-    }
+    });
 
     write_ppm(&buffer, (camera.width, camera.height), "test2.ppm");
 }
